1f49287c9a61b7d55c4d2bac48fdc95098e7daab
arturmartins <arturmartinssilva078@gmail.com>
2025-10-30 10:29:07 -0300

    feat: adição do módulo de autenticação ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
 import { TarefaModule } from './tarefa/tarefa.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DbModule, TarefaModule, UsuarioModule],
+  imports: [DbModule, TarefaModule, UsuarioModule, AuthModule],
   controllers: [],
   providers: [],
 })
